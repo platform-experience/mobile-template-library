5f1e76235bda1dbaa750dde2d355f446c9e59b4d
Steven Gregory <sgregory@gmail.com>
2019-04-18 10:18:41 -0500

    Created shell automation scripts

---
+++ scripts/build.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+source ./scripts/config.sh
+source ./scripts/messages.sh
+
+branch_checkout() {
+  local branch=feature/${PREFIX}${MO}
+  git fetch origin
+  if [[ $(git branch --list ${branch}) ]]; then
+    echo -e "${BLUE}${BRANCH_SWITCH_MSG}${RESET}"
+    git checkout ${branch}
+  else
+    echo -e "${BLUE}${BRANCH_CREATE_MSG}${RESET}"
+    git checkout -b ${branch} origin/master
+  fi
+}
+
+create_base_dir() {
+  echo -e "${BLUE}${SCAFFOLD_MSG}${RESET}"
+  make_core_dir ${SRC_DIR}
+  make_core_dir ${PREFIX}${MO}
+  mkdir ${IMAGE_DIR}
+  curl ${CONFIG_GIST} > config.json
+  curl ${README_GIST} > README.md
+  touch ${PREFIX}${MO}.${UPDATE_SET}
+}
+
+create_option_dir() {
+  mkdir ${1}
+  touch ${1}/${PREFIX}${MO}.${2}
+}
+
+fetch_github_user() {
+  if [[ $(git config user.name) ]]; then
+    echo $(git config user.name)
+  else
+    echo ${CONTRIB_TEMP}
+  fi
+}
+
+format_data() {
+  echo $(printf "%s" $1)
+}
+
+has_dashes() {
+  if [[ ${args[0]} == *-* ]]; then
+    name_has_dashes=true
+  fi
+}
+
+main() {
+  get_options
+  set_name
+  branch_checkout
+  create_base_dir
+  has_dashes
+  sub_base_content
+  make_commit
+}
+
+make_commit() {
+  echo -e "${BLUE}${COMMIT_STATUS_MSG}${RESET}"
+  cd ../../
+  git add ${SRC_DIR}/${PREFIX}${MO}
+  git commit -a -m "${COMMIT_MSG} ${mo_name_lower}"
+  echo -e "${GREEN}${DONE_MSG}${RESET}"
+}
+
+make_core_dir() {
+  if [[ -d $1 ]]; then
+    cd $1
+  else
+    mkdir $1 && cd $1
+  fi
+}
+
+make_lowercase() {
+  echo $(tr '[:lower:]' '[:lower:]' <<< ${i:0:1})${i:1}"$(make_space $1)"
+}
+
+make_space() {
+  if [[ $1 ]]; then
+    echo " "
+  fi
+}
+
+make_uppercase() {
+  echo $(tr '[:lower:]' '[:upper:]' <<< ${i:0:1})${i:1}"$(make_space $1)"
+}
+
+replace_content() {
+  sed -i '' -e "s/${1}/${2}/g" ${3}
+}
+
+set_name() {
+  local mo_dir=()
+  for i in "${inputs[@]}"; do
+    mo_dir+=$(echo -${i} | tr '[:upper:]' '[:lower:]')
+    mo_name_upper+=$(make_uppercase ${i})
+    mo_name_lower+=$(make_lowercase ${i})
+  done
+  MO=$(format_data ${mo_dir[@]})
+}
+
+sub_base_content() {
+  echo -e "${BLUE}${UPDATE_MSG}${RESET}"
+  if [[ ${name_has_dashes} ]]; then
+    local dash_readme=()
+    rm=${mo_name_upper}
+    IFS='-' read -ra content <<< "$rm"
+    for i in "${content[@]}"; do
+      dash_readme+=$(make_uppercase ${i})
+    done
+    replace_content "${NAME_TEMP}" "${dash_readme%??}" README.md
+    replace_content "${NAME_TEMP}" "${dash_readme%??}" config.json
+  else
+    replace_content "${NAME_TEMP}" "${mo_name_upper%?}" README.md
+    replace_content "${NAME_TEMP}" "${mo_name_upper%?}" config.json
+  fi
+  replace_content "${CONTRIB_TEMP}" "$(fetch_github_user)" config.json
+  replace_content "${DIR_TEMP}" "${PREFIX}${MO}" config.json
+  replace_content "${DIR_TEMP}" "${PREFIX}${MO}" README.md
+}
+
+get_options() {
+  echo -e "${BLUE}${START_MSG}${RESET}"
+  inputs=()
+  for i in "${args[@]}"; do
+    inputs+=("$i")
+  done
+}
+
+args=($@)
+main
---
+++ scripts/config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# directories
+IMAGE_DIR=images
+SRC_DIR=src
+
+# extensions
+UPDATE_SET=u-update-set.xml
+
+# gists
+CONFIG_GIST=https://gist.githubusercontent.com/stevengregory/ae47bc97a14c5196696688668b86a00b/raw/a91d27b3e510ad2c87f4e6f73a7edd1aa25f07b9/config.json
+README_GIST=https://gist.githubusercontent.com/stevengregory/ea48bb1cd6726a7768d9bca162fea6c3/raw/372c8a4e07a056cb57a16af00107090c81b8a3a3/README.md
+
+# prefix
+PREFIX=mo
+
+# temp content
+CONTRIB_TEMP='Sush Chandrashekar'
+DIR_TEMP='temp-name'
+NAME_TEMP='Temp Name'
